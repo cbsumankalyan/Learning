1e6fc2a1681580b7df47088fba395518986a59e6
sumancb <sumancb@UNI-BGL-SUMAN.corp.unicity.net>
2021-05-12 11:05:44 +0530

    keys

--- src/main/java/Tests/KeyBoardKeys.java
+++ src/main/java/Tests/KeyBoardKeys.java
@@ -1,17 +1,19 @@
 package Tests;
 
 import org.testng.annotations.Test;
+
+import POM.Start;
 import Pages.SuperTestNG;
 
 public class KeyBoardKeys extends SuperTestNG {
 	
 	@Test
-	public void Keys() {
+	public void Keys() throws InterruptedException {
+		
+		Start start = new Start(driver);
 		
 		childtest =keys.createNode("<b><font color=407899>"+"Key Board Keys" + "</font></b>");
-		
-		
-		
+		start.KeyBoard();
 	}
 
 }
